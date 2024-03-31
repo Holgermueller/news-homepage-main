abc8456e261d83da9de5a0e3d05c9acc945af617
holgermueller <9jholger@gmail.com>
2024-03-31 15:11:58 -0500

    styled mobile nav

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 "use strict";
+
+function openNav() {}
+
+function closeNav() {
+  document.getElementById("closeNav");
+  console.log("click");
+  document.getElementById("mobileNav").style.width = "none";
+}
